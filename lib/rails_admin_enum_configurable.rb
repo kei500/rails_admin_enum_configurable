a4c875e3f4de1113941d801e6dd2b6a52ba5f082
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2018-05-13 11:10:29 +0900

    declare _enum method dynamically

--- lib/rails_admin_enum_configurable.rb
+++ lib/rails_admin_enum_configurable.rb
@@ -1,5 +1,38 @@
 require "rails_admin_enum_configurable/version"
 
 module RailsAdminEnumConfigurable
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def method_missing(name)
+      if name.to_s.end_with?('_enum') && respond_to?(name.to_s.match(/(.+)_enum/)[1].pluralize)
+        attribute = name.to_s.match(/(.+)_enum/)[1]
+        if self.send(attribute.pluralize).is_a?(::Hash)
+          self.send(attribute.pluralize).keys.map{|k| [I18n.t("enums.#{self.to_s.underscore}.#{attribute}.#{k}"), k]}.to_h
+        elsif self.send(attribute.pluralize).is_a?(::Array)
+          self.send(attribute.pluralize).map{|k| I18n.t("enums.#{self.to_s.underscore}.#{attribute}.#{k}")}
+        else
+          raise "Unknown enum type. attribute: #{attribute}, type: #{self.send(attribute.pluralize).class}"
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to?(name, include_all = false)
+      if name.to_s.end_with?('_enum') && respond_to?(name.to_s.match(/(.+)_enum/)[1].pluralize)
+        true
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(name, include_all = false)
+      if name.to_s.end_with?('_enum') && respond_to?(name.to_s.match(/(.+)_enum/)[1].pluralize)
+        true
+      else
+        super
+      end
+    end
+  end
 end
